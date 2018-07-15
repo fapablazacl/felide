b158f79a21de8d8031bc3faa19c7363a1890e38a
Felipe Apablaza <ing.apablaza@gmail.com>
2018-07-14 22:00:20 -0400

    Added designs in UML

---
+++ src/felide.core/felide/pom/Workspace.hpp
@@ -0,0 +1,14 @@
+
+#ifndef __FELIDE_POM_WORKSPACE_HPP__
+#define __FELIDE_POM_WORKSPACE_HPP__
+
+namespace felide {
+    class Project;
+    class Workspace {
+    public:
+        
+
+    };
+}
+
+#endif
---
+++ src/sandbox/sandbox.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+
+namespace felide {
+    class Workspace {
+    public:
+
+    };
+
+    class Project {
+    public:
+
+    };
+
+    class ProjectItem {
+    public:
+        virtual ~ProjectItem() {}
+
+    };
+}
+
+int main(int argc, char **argv) {
+
+    return 0;
+}
