8c440533ea37e931d5224203c8932c40197a5723
Felipe Apablaza <ing.apablaza@gmail.com>
2018-09-17 22:59:13 -0300

    Removed unused methods from MainWindowView

--- src/felide.ui.qt5/felide/ui/MainWindowView.hpp
+++ src/felide.ui.qt5/felide/ui/MainWindowView.hpp
@@ -2,21 +2,12 @@
 #ifndef __FELIDE_UI_MAINWINDOWVIEW_HPP__
 #define __FELIDE_UI_MAINWINDOWVIEW_HPP__
 
-#include <string>
-#include <boost/optional.hpp>
-#include <vector>
-#include <map>
-
 namespace felide {
     class EditorManagerView;
     class DialogManagerView;
     class MainWindowView {
     public:
         virtual ~MainWindowView();
-
-        virtual boost::optional<std::string> showDialogModal(const DialogViewData &dialogViewData) = 0;
-        
-        virtual boost::optional<bool> showAskModal(const std::string &title, const std::string &message) = 0;
 
         virtual EditorManagerView* getEditorManagerView() = 0;
 
