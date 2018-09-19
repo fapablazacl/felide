983963e9658af0209334662cdc609e73022b1463
Felipe Apablaza <ing.apablaza@gmail.com>
2018-09-19 15:42:34 -0300

    Fixed errors with dynamic plugin loading

--- src/felide.core/felide/system/PluginManager.cpp
+++ src/felide.core/felide/system/PluginManager.cpp
@@ -1,11 +1,13 @@
 
 #include "PluginManager.hpp"
 
-#include <exception>
+#include <stdexcept>
 #include <iostream>
 #include <boost/dll.hpp>
 
 namespace felide {
+
+
     PluginManager::PluginManager(Core *core) {
         m_core = core;
     }
@@ -13,20 +15,27 @@ namespace felide {
     PluginManager::~PluginManager() {}
 
     void PluginManager::loadPlugin(const std::string &name) {
+        // TODO: Export this to a configuration file
+        const std::string pluginPath = "/usr/local/lib/";
+
         typedef felide::Plugin* (*PluginCreateProc);
 
         try {
             std::cout << "Loading " << name << std::endl;
 
             auto felide_plugin_create = boost::dll::import<PluginCreateProc> (
-                name,                     
-                "felide_plugin_create", 
+                pluginPath + name,
+                "felide_plugin_create",
                 boost::dll::load_mode::append_decorations
             );
 
+            if (!felide_plugin_create) {
+                throw std::runtime_error("Required entry point doesn't exist.");
+            }
+
             std::cout << "Load OK " << name << std::endl;
         } catch (const std::exception &exp) {
-            std::cout << exp.what() << std::endl;
+            std::cout << "Load failed:" << std::endl << exp.what() << std::endl;
         }
     }
 }
