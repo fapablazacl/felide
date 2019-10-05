623867eee2dff195b6012c44fb3fdd38b8066134
Felipe Apablaza <ing.apablaza@gmail.com>
2019-10-05 13:07:50 -0300

    Added initial AppDelegate

---
+++ src/felide.gui.cocoa/AppDelegate.m
@@ -0,0 +1,18 @@
+
+#import "AppDelegate.h"
+#import <AppKit/AppKit.h>
+
+@interface AppDelegate()
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+- (void) applicationDidFinishLaunching: (NSNotification *) notification {
+
+}
+
+- (void) applicationWillTerminate: (NSNotification *) notification {
+    
+}
+
+@end
--- src/felide.gui.cocoa/Main.mm
+++ src/felide.gui.cocoa/Main.mm
@@ -1,6 +1,17 @@
 
 #import <Cocoa/Cocoa.h>
+#import "AppDelegate.h"
 
-int main(int argc, const char * argv[]) {
-    return NSApplicationMain(argc, argv);
+int main(int argc, const char *argv[]) {
+    NSArray *topLevelObjects = nil;
+
+    NSApplication *app = [NSApplication sharedApplication];
+    [[NSBundle mainBundle] loadNibNamed: @"MainMenu" owner: app topLevelObjects: &topLevelObjects];
+
+    AppDelegate *appDelegate = [[AppDelegate alloc] init];
+
+    [app setDelagate: appDelegate];
+    [app run];
+
+    return 0;
 }
