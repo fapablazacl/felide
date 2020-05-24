82a1219e2053b5e5a6c9411150448971ac6a8eb0
Felipe Apablaza <ing.apablaza@gmail.com>
2020-05-23 20:23:28 -0400

    Removed unused files

--- pocs/form-designer/FormDesignerApp.cpp
+++
@@ -1,8 +0,0 @@
-
-#include <Windows.h>
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, INT nCmdShow) {
-    ::MessageBoxW(NULL, L"Hello, World!", L"Form Designer", MB_OK);
-	
-	return 0;
-}
